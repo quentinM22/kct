41f0d904dfe00a5e5f22025a859667c8cb1931f1
quentinM22 <quentin.meunier1@laposte.net>
2022-11-02 20:57:14 +0100

    API User

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -8,22 +8,25 @@ require('dotenv').config()
 const signup = async (req, res, next) => {
     try {
         const dataBody = req.body
-        const passwordHash = await bcrypt.hash(dataBody.password, 10)
-        const newUser = await User.create({
-            firstName: dataBody.firstName,
-            lastName: dataBody.lastName,
-            email: dataBody.email,
-            password: passwordHash
-        })
-        res.status(200).json({ message: `Utilisateur ${newUser.firstName} ${newUser.lastName} créé!` })
-
+        if (dataBody.password.length < 8) {
+            return res.status(405).json({ message: "Mots de passe trop court" })
+        }
+            const passwordHash = await bcrypt.hash(dataBody.password, 10)
+            const newUser = await User.create({
+                firstName: dataBody.firstName,
+                lastName: dataBody.lastName,
+                email: dataBody.email,
+                password: passwordHash
+            })
+            res.status(200).json({ message: `Utilisateur ${newUser.firstName} ${newUser.lastName} créé!` })
+        
     } catch (error) {
         res.status(500).json({ error })
     }
 }
 
 //connexion user 
-const login = async(req, res, next) => {
+const login = async (req, res, next) => {
     try {
         const dataBody = req.body
         const user = await User.findOne({ email: dataBody.email })
@@ -31,14 +34,14 @@ const login = async(req, res, next) => {
             return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
         }
         try {
-           const valid = await bcrypt.compare(dataBody.password, user.password) 
-           if(!valid){
-            return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
-           }
+            const valid = await bcrypt.compare(dataBody.password, user.password)
+            if (!valid) {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+            }
         } catch (error) {
             res.status(500).json({ error })
         }
-        
+
         res.status(200).json({
             userId: user._id,
             token: jwt.sign(
@@ -52,7 +55,7 @@ const login = async(req, res, next) => {
             User.findOne({ email: req.body.email })
                 .then(user => {
                     if (!user) {
-                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
                     }
                     bcrypt.compare(req.body.password, user.password)
                         .then(valid => {
@@ -67,7 +70,7 @@ const login = async(req, res, next) => {
                         .catch(error => res.status(500).json({ error }));
                 })
                 .catch(error => res.status(500).json({ error }));
-         };
+        };
     }
 
 }
--- backend/models/User.Model.js
+++ backend/models/User.Model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     firstName:{type: String, required : true},
     lastName: {type: String, required : true},
     email: {type: String, required : true, unique: true},
-    password: {type: String, required : true},
+    password: {type: String, required : true, min : 8},
 },{timestamps: true })
 
 userSchema.plugin(uniqueValidator);
