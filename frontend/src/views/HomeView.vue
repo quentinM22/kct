b6e63d0166640a830ebe3b0c678acc1a93e0bc3d
quentinM22 <quentin.meunier1@laposte.net>
2022-11-14 17:10:42 +0100

    Frontend: Vuejs

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,74 @@
+<script setup>
+import NavbarHome from '../components/NavbarHome.vue'
+</script>
+
+<template>
+  <header>
+
+    <main class="wrapper">
+      <div class="page-header clear-filter" filter-color="orange">
+
+        <div class="page-header-image" data-parallax="true"
+          style="background-image:url('/pexels-belle-co-402028.jpg');">
+
+        </div>
+        <NavbarHome />
+        <div class="container">
+
+          <div class="content-center brand">
+            <img class="n-logo" src="./assets/img/now-logo.png" alt="">
+            <h1 class="h1-seo">Le Karaté Club du Trégor</h1>
+            <h3>Vous souhaite la Bienvenue sur son site.</h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="section section-about-us">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 ml-auto mr-auto text-center">
+              <h2 class="title">Who we are?</h2>
+              <h5 class="description">According to the National Oceanic and Atmospheric Administration, Ted, Scambos,
+                NSIDClead scentist, puts the potentially record low maximum sea ice extent tihs year down to low ice
+                extent in the Pacific and a late drop in ice extent in the Barents Sea.</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="main">
+        <div class="section section-images">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6 text-center">
+                <div class="card" style="width: 20rem;">
+                  <img class="card-img-top" src="..." alt="Card image cap">
+                  <div class="card-body">
+                    <h4 class="card-title">Le DOJO de Tréguier</h4>
+                    <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">Info</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 text-center">
+                <div class="card" style="width: 20rem;">
+                  <img class="card-img-top" src="..." alt="Card image cap">
+                  <div class="card-body">
+                    <h4 class="card-title">Le DOJO de Plouha</h4>
+                    <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">Info</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </header>
+</template>
+
+<script>
+fetch("http://127.0.0.1:3000/api/post/getAllPost/")
+.then(res=>console.log(res.json()))
+// .then(res=>console.log(res.value))
+</script>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,70 @@
+<template>
+<main class="login-page sidebar-collapse">
+
+<div class="page-header clear-filter" filter-color="orange">
+
+    <div class="page-header-image" style="background-image:url(/pexels-photo-96423.jpeg)"></div>
+
+    <div class="content">
+        <h2 class="mt-5 text-center text-light">Connexion Administration</h2>
+        <div class="container">
+            <div class="col-md-4 ml-auto mr-auto">
+                <div class="card card-login card-plain">
+                    <form class="form" @submit.prevent="Login">
+
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" v-model="email" placeholder="test@test.fr">
+                                <small id="emailHelp" class="form-text text-muted">We'll never share your email
+                                    with anyone else.</small>
+                            </div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1"
+                                    v-model="password" placeholder="********">
+                            </div>
+                        </div>
+                        <div class="card-footer text-center">
+                            <input class="btn btn-primary btn-round btn-lg btn-block" type="submit"
+                                value="Connexion">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</main>
+</template>
+    
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter()
+const email = ref('')
+const password = ref('')
+const Login = async () => {
+    const res = await fetch("http://127.0.0.1:3000/api/user/login", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email.value,
+            password: password.value
+        })
+    }).then(res => res.json())
+    if (res.success) {
+        localStorage.setItem('token', res.token)
+        console.log(res.token);
+        router.push('/dashboard')
+    } else {
+        alert(res.message)
+    }
+}
+
+
+</script>
